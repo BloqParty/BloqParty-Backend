1837c8abb559651b793d11fbb78c4e880eac62e0
Syl <hi@sylviiu.dev>
2023-09-11 17:07:40 -0500

    add "default" to schema options

--- src/middleware/verifySchema.js
+++ src/middleware/verifySchema.js
@@ -4,53 +4,65 @@ module.exports = (schema, schemaType) => (req, res, next) => {
     const notMatching = {};
 
     for(const [key, match] of Object.entries(schema).map(o => [o[0], Object.assign({}, o[1])])) {
-        const original = req[schemaType][key]
+        let original = req[schemaType][key];
 
-        switch(match.type) {
-            case `integer`:
-                req[schemaType][key] = parseInt(req[schemaType][key]);
+        const provided = (typeof req[schemaType][key] !== `undefined` && !(typeof req[schemaType][key] == `object` && !req[schemaType][key]))
 
-                if(!isNaN(req[schemaType][key])) {
-                    match.type = `number`;
-                } else {
-                    req[schemaType][key] = original;
-                }
+        const reasons = [];
 
-                break;
-            case `float`:
-                req[schemaType][key] = parseFloat(req[schemaType][key]);
+        // if undefined or null and required
+        if(!provided && match.required) {
+            reasons.push({
+                isRequired: true,
+                provided: false,
+            })
+        } else if(match.required || provided) {
+            switch(match.type) {
+                case `integer`:
+                    req[schemaType][key] = parseInt(req[schemaType][key]);
+    
+                    if(!isNaN(req[schemaType][key])) {
+                        match.type = `number`;
+                    } else {
+                        req[schemaType][key] = original;
+                    }
+    
+                    break;
+                case `float`:
+                    req[schemaType][key] = parseFloat(req[schemaType][key]);
+    
+                    if(!isNaN(req[schemaType][key])) {
+                        match.type = `number`;
+                    } else {
+                        req[schemaType][key] = original;
+                    }
+    
+                    break;
+                case `boolean`:
+                    req[schemaType][key] = (req[schemaType][key] === `true` ? true : req[schemaType][key] === `false` ? false : req[schemaType][key]);
+                    break;
+            };
 
-                if(!isNaN(req[schemaType][key])) {
-                    match.type = `number`;
-                } else {
-                    req[schemaType][key] = original;
-                }
-
-                break;
-            case `boolean`:
-                req[schemaType][key] = (req[schemaType][key] === `true` ? true : req[schemaType][key] === `false` ? false : req[schemaType][key]);
-                break;
+            if(typeof req[schemaType][key] !== match.type) {
+                reasons.push({
+                    expectedType: match.type,
+                    gotType: `${typeof original}`,
+                })
+            };
+    
+            if(Array.isArray(match.values) && !match.values.includes(req[schemaType][key])) {
+                reasons.push({
+                    expectedValue: match.values,
+                    gotValue: typeof req[schemaType][key] == `undefined` ? `undefined` : req[schemaType][key],
+                })
+            };
+        } else if(!provided && match.default) {
+            original = match.default;
         }
 
-        if(typeof req[schemaType][key] !== match.type) {
-            notMatching[key] = {
-                ...notMatching[key] || {},
-                expectedType: match.type,
-                gotType: `${typeof original}`,
-            }
-        };
-
-        if(Array.isArray(match.values) && !match.values.includes(req[schemaType][key])) {
-            notMatching[key] = {
-                ...notMatching[key] || {},
-                expectedValue: match.values,
-                gotValue: typeof req[schemaType][key] == `undefined` ? `undefined` : req[schemaType][key],
-            }
-        };
-
-        if(notMatching[key]) Object.assign(notMatching[key], {
-            value: original
-        })
+        if(reasons.length > 0) {
+            notMatching[key] = reasons.reduce((a,b) => Object.assign(a, b), {});
+        }
 
         req[schemaType][key] = original;
     };
