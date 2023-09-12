09dc3d2179a71e02e22285bf396eee8eff01d699
Syl <hi@sylviiu.dev>
2023-09-12 13:29:44 -0500

    fixed avatar uploading behind nginx proxy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const os = require('os');
 const http = require('http');
 const express = require('express');
+const superagent = require('superagent');
 const swaggerUi = require('swagger-ui-express');
 const httpProxy = require('http-proxy');
 const thread = require(`./balancer/thread`);
@@ -45,24 +46,67 @@ const proxyServer = httpProxy.createProxyServer({});
 
 console.log(`Started ${threadKeys.length} threads (ports ${threadKeys.join(`, `)}); creating proxy`);
 
+proxyServer.on(`error`, (err, req, res) => {
+    console.error(`Proxy error:`, err);
+
+    res.writeHead(500, {
+        'Content-Type': 'text/plain'
+    });
+
+    res.end(`Something went wrong. Please try again later.`);
+});
+
+const methodsWithBody = [`POST`, `PUT`, `PATCH`];
+
 const app = express();
+const server = http.createServer(async (req, res) => {
+    if(req.url.startsWith(`/docs`)) {
+        return app(req, res);
+    } else {
+        let thisThread = threadUsed++;
+    
+        const port = threadKeys[thisThread] || threadKeys[thisThread = threadUsed = 0];
 
-app.use(`/docs`, (req, res, next) => {
-    console.log(`[DOCS] ${req.url}`);
-    next();
-}, swaggerUi.serve, swaggerUi.setup(await swaggerObj));
+        console.log(`Proxying ${req.url.split(`?`)[0]} to port ${port}`);
 
-app.use((req, res) => {
-    let thisThread = threadUsed++;
-    if(!threadKeys[thisThread]) thisThread = threadUsed = 0;
+        if(methodsWithBody.includes(req.method.toUpperCase())) await new Promise(async res => {
+            let data = ``;
 
-    const port = threadKeys[thisThread];
+            req.on(`data`, chunk => {
+                if(typeof data == `string`) {
+                    data += chunk.toString();
 
-    console.log(`Proxying to port ${port}`);
+                    try {
+                        data = JSON.parse(data);
+                        req.body = data;
+                        console.log(`[BODY] parsed`);
+                        res();
+                    } catch(e) {
+                        console.log(`[BODY] ${data.length}...`);
+                    }
+                }
+            });
+        });
 
-    proxyServer.web(req, res, { target: `http://localhost:${port}` });
-})
+        const request = superagent[req.method.toLowerCase()](`http://localhost:${port}` + req.url)
+        
+        if(req.body) request.send(req.body);
+        if(req.headers) request.set(req.headers);
+        
+        request.then(response => {
+            res.writeHead((response.statusCode || response.status), response.headers);
+            res.end(response.text);
+        });
+    }
+});
 
-app.listen(PORT, () => {
+swaggerObj.then(json => {
+    app.use(`/docs`, (req, res, next) => {
+        console.log(`[DOCS] ${req.url}`);
+        next();
+    }, swaggerUi.serve, swaggerUi.setup(json));
+});
+
+server.listen(PORT, () => {
     console.log(`Started proxy on port ${PORT}`);
 });
