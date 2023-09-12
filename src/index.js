8fd2d1f859c655e29fc3eabff0dd1ab101c95c3c
Syl <hi@sylviiu.dev>
2023-09-12 10:59:32 -0500

    render docs on proxy balancer thread

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 const os = require('os');
 const http = require('http');
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 const httpProxy = require('http-proxy');
 const thread = require(`./balancer/thread`);
+const swagger = require(`./utils/swagger`)
 
 const PORT = 9999;
 
 const threadCount = parseInt(process.argv[2]) || os.cpus().length;
 console.log(`Starting ${threadCount} threads (received: ${process.argv[2]})`);
 
+let swaggerResolve = null;
+let swaggerObj = new Promise(res => {
+    swaggerResolve = res;
+});
+
 const threads = {};
 let threadUsed = 0;
 
-for(const port of Array.from(Array(threadCount).keys()).map(a => a+1+PORT)) await new Promise(async res => {
+for(const [ port, index ] of Array.from(Array(threadCount).keys()).map((a, i) => ([a+1+PORT, i]))) await new Promise(async res => {
     console.log(`Starting thread ${port}`);
 
     const data = await thread(`./thread.js`, { PORT: port });
@@ -20,6 +28,15 @@ for(const port of Array.from(Array(threadCount).keys()).map(a => a+1+PORT)) awai
 
     console.log(`Started thread ${port}`);
 
+    if(index == 0) {
+        console.log(`Generating swagger`);
+
+        swagger({ port }).then(json => {
+            console.log(`Generated swagger`);
+            swaggerResolve(json);
+        })
+    };
+    
     res();
 });
 
@@ -28,7 +45,14 @@ const proxyServer = httpProxy.createProxyServer({});
 
 console.log(`Started ${threadKeys.length} threads (ports ${threadKeys.join(`, `)}); creating proxy`);
 
-http.createServer((req, res) => {
+const app = express();
+
+app.use(`/docs`, (req, res, next) => {
+    console.log(`[DOCS] ${req.url}`);
+    next();
+}, swaggerUi.serve, swaggerUi.setup(await swaggerObj));
+
+app.use((req, res) => {
     let thisThread = threadUsed++;
     if(!threadKeys[thisThread]) thisThread = threadUsed = 0;
 
@@ -37,6 +61,8 @@ http.createServer((req, res) => {
     console.log(`Proxying to port ${port}`);
 
     proxyServer.web(req, res, { target: `http://localhost:${port}` });
-}).listen(PORT, () => {
+})
+
+app.listen(PORT, () => {
     console.log(`Started proxy on port ${PORT}`);
 });
