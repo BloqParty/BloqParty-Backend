9a10fe2a1a57c7ee5b66fca6692cfa8c9eb31530
Syl <hi@sylviiu.dev>
2023-09-14 03:11:06 -0500

    fetch userPermakey by user first, then match auth

--- src/middleware/userPermakey.js
+++ src/middleware/userPermakey.js
@@ -7,18 +7,19 @@ module.exports = ({
     }
 }={}) => (req, res, next) => {
     if(!req.headers.authorization) return res.status(code).send(message);
+    if(!req.body?.id) return res.status(code).send(message);
 
-    Models.users.findOne({ apiKey: req.headers.authorization }).then(doc => {
+    Models.users.findOne({ game_id: req.body.id }).then(doc => {
         req.user = doc?.toObject();
 
         if(!doc) {
             console.log(`[auth by permakey] user not found`);
             res.status(code).send(message);
-        } else if(req.user.game_id !== req.body.id) {
-            console.log(`[auth by permakey] body id ${req.body.id} doesn't match key's user id ${req.user.game_id}`);
+        } else if(req.headers.authorization !== req.user.apiKey) {
+            console.log(`[auth by permakey] body key ${req.headers.authorization} doesn't match user's key ${req.user.apiKey}`);
             res.status(code).send(message);
         } else {
-            console.log(`[auth by permakey] user found`);
+            console.log(`[auth by permakey] user found (${req.body.id}, ${req.user.game_id}), and apikey matches (${req.user.apiKey.slice(0, 8)}..., ${req.headers.authorization.slice(0, 8)}...)`);
             next();
         }
     });
