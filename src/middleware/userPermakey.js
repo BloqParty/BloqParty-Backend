3e58a32a93029866044ea10d570ec4edab530e64
Syl <hi@sylviiu.dev>
2023-09-18 13:10:00 -0500

    add 401 status code if not given

--- src/middleware/userPermakey.js
+++ src/middleware/userPermakey.js
@@ -6,18 +6,20 @@ module.exports = ({
         error: `Unauthorized.`
     }
 }={}) => (req, res, next) => {
-    if(!req.headers.authorization) return res.status(code).send(message);
-    if(!req.body?.id) return res.status(code).send(message);
+    const useCode = Number(code) || 401;
+
+    if(!req.headers.authorization) return res.status(useCode).send(message);
+    if(!req.body?.id) return res.status(useCode).send(message);
 
     Models.users.findOne({ game_id: req.body.id }).then(doc => {
         req.user = doc?.toObject();
 
         if(!doc) {
             console.log(`[auth by permakey] user not found`);
-            res.status(code).send(message);
+            res.status(useCode).send(message);
         } else if(req.headers.authorization !== req.user.apiKey) {
             console.log(`[auth by permakey] body key ${req.headers.authorization} doesn't match user's key ${req.user.apiKey}`);
-            res.status(code).send(message);
+            res.status(useCode).send(message);
         } else {
             console.log(`[auth by permakey] user found (${req.body.id}, ${req.user.game_id}), and apikey matches (${req.user.apiKey.slice(0, 8)}..., ${req.headers.authorization.slice(0, 8)}...)`);
             next();
