97048822d924fe9d92b90e6ef4c6606cab345619
NuggoDEV <NuggoDEV@gmail.com>
2023-09-21 00:56:17 +0000

    modify description

--- src/endpoints/Server/testers.js
+++ src/endpoints/Server/testers.js
@@ -1,5 +1,5 @@
 module.exports = {
     path: `/testers`,
-    description: `Displays a list of staff IDs`,
+    description: `Displays a list of tester IDs`,
     get: async (req, res) => res.send(await Bun.file('./src/extras/testers.txt').text())
 };
