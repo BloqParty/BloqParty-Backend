d179ea2f5cee02f7b15f2bb1ea46efb2fa1a908a
Syl <hi@sylviiu.dev>
2023-09-25 21:49:22 -0500

    create recent endpoint

---
+++ src/endpoints/Leaderboard/recent.js
@@ -0,0 +1,39 @@
+const leaderboard = require('../../utils/leaderboard');
+
+module.exports = {
+    path: `/leaderboard/recent`,
+    description: `Gets posted scores from a specific map hash`,
+    query: {
+        page: {
+            type: `integer`,
+            description: `Page of scores to get`,
+            required: false,
+            default: 1
+        },
+        limit: {
+            type: `integer`,
+            description: `Amount of scores to return. Max 50`,
+            values: Array.from(Array(50).keys()).map(a => a+1), // 1-50
+            required: false,
+            default: 10
+        },
+    },
+    get: async (req, res) => {
+        leaderboard.getRecent({ ...req.query }).then(data => {
+            res.send(data);
+        }).catch(e => {
+            const s = e.toString().toLowerCase().split(` `)
+
+            res.send(`yeah, no`)
+
+            //res.status(s.includes(`found`) ? 404 : 500).send({ error: e });
+        })
+    }
+}
+
+module.exports.get.tests = [
+    {
+        path: `/leaderboard/recent`,
+        description: `Successful leaderboard lookup`
+    },
+]
