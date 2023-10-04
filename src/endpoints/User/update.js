f1a98b06603932b3997c46351e356938f485a807
Nuggo <nuggodev@gmail.com>
2023-10-04 14:06:41 +1030

    Update user endpoint

---
+++ src/endpoints/User/update.js
@@ -0,0 +1,41 @@
+const user = require("../../utils/user");
+
+module.exports = {
+    path: "/user/:id/update",
+    description: "Updates the users profile",
+    params: {
+        id: {
+            type: `string`,
+            description: `The user's game ID`,
+            required: true
+        }
+    },
+    middleware: {
+        websiteKey: require(`../../middleware/websiteKey`)({
+            message: `nah 🔥`
+        }),
+    },
+    body: {
+        username: {
+            type: `string`,
+            required: false,
+        },
+        description: {
+            type: `string`,
+            required: false,
+        },
+        avatar: {
+            type: `string`,
+            description: `Base64-encoded image data (MUST BE A PNG)`,
+            required: false,
+        }
+    },
+    post: async (req, res) => {
+        user.update(req.body, req.params.id).catch(e => console.log(e)).then(usr => {
+            if (usr !== null)
+                res.status(200).send("Updated user");
+            else 
+                res.status(404).send("User not found");
+        })
+    }
+}
