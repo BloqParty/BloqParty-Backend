21ffed5d7c4bc30d168a34814c3090c0b4f5c11b
NuggoDEV <NuggoDEV@gmail.com>
2023-10-16 20:14:11 +0000

    Get users from staff and testers files

--- src/endpoints/Server/testers.js
+++ src/endpoints/Server/testers.js
@@ -1,5 +1,10 @@
 module.exports = {
     path: `/testers`,
     description: `Displays a list of tester IDs`,
-    get: async (req, res) => res.send(await Bun.file('./src/extras/testers.txt').text())
+    get: async (req, res) => {
+        const testers = await Bun.file('./src/extras/testers.txt').text();
+        const staff = await Bun.file(`./src/extras/staff.txt`).text();
+
+        res.send(staff + testers);
+    }
 };
