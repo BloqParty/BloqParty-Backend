54123c1adddcd9eaf9e69ef0a68ef2cca52290fe
Nuggo <nuggodev@gmail.com>
2023-11-05 14:53:12 +1030

    Use Promise.all (I think I did it right)

--- src/endpoints/Server/search.js
+++ src/endpoints/Server/search.js
@@ -19,31 +19,21 @@ module.exports = {
             leaderboards: []
         };
 
-        await Models.users.find({ username: { $regex: value, $options: "i" } }).then(docs => {
-            for (var doc of docs) 
-                results.users.push(strip(doc));
-        });
+        const [ usernames, gameIDs, discordIDs, leaderboards ] = await Promise.all([
+            Models.users.find({ username: { $regex: value, $options: "i" } }),
+            Models.users.find({ gameID: { $regex: value, $options: "i" } }),
+            Models.users.find({ discordID: { $regex: value, $options: "i" } }),
+            Models.leaderboards.find({ name: { $regex: value, $options: "i" } })
+        ])
 
-        await Models.users.find({ gameID: { $regex: value, $options: "i" } }).then(docs => {
-            for (var doc of docs)
-                results.users.push(strip(doc));
-        });
-
-        await Models.users.find({ discordID: { $regex: value, $options: "i" } }).then(docs => {
-            for (var doc of docs)
-                results.users.push(strip(doc));
-        });
-
-        await Models.leaderboards.find({ "name": { $regex: value, $options: "i" } }).then(docs => {
-            for (var doc of docs)
-            {
-                var leaderboard = {
-                    name: doc.name,
-                    hash: doc.hash
-                }
-                results.leaderboards.push(leaderboard);
-            }
-        });
+        for (const user of usernames)
+            results.users.push(strip(user));
+        for (const user of gameIDs)
+            results.users.push(strip(user));
+        for (const user of discordIDs)
+            results.users.push(strip(user));
+        for (const leaderboard of leaderboards)
+            results.leaderboards.push({ name: leaderboard.name, hash: leaderboard.hash });
 
         (results.users.length > 0 || results.leaderboards.length > 0) ? res.send(results) : res.send({error: "No results found"});
     }
