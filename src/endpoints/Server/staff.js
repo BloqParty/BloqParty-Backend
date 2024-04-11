7a04c88da4d8e458f750ea0e44da0e786322a2b5
Riley Prince <NuggoDEV@gmail.com>
2024-04-12 03:48:44 +0930

    recommit everything again

--- src/endpoints/Server/staff.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    path: `/staff`,
-    description: `Displays a list of staff IDs`,
-    get: async (req, res) => res.send(await Bun.file('./src/extras/staff.txt').text())
-};
--- src/index.js
+++ src/index.js
@@ -1,143 +1,40 @@
-require('dotenv').config();
+require("dotenv").config();
+const express = require("express");
+const fs = require("fs");
 
-const os = require('os');
-const http = require('http');
-const express = require('express');
-const swaggerUi = require('swagger-ui-express');
-const thread = require(`./balancer/thread`);
-const swagger = require(`./utils/swagger`);
-
-const threadCount = parseInt(process.argv[2]) || os.cpus().length;
-console.log(`[Server | Startup] Starting server with ${threadCount}.`);
-
-let swaggerResolve = null;
-let swaggerObj = new Promise(res => {
-    swaggerResolve = res;
-});
-
-const threads = {};
-let threadUsed = 0;
-
-for(const [ port, index ] of Array.from(Array(threadCount).keys()).map((a, i) => ([a+1+parseInt(process.env.PORT), i]))) await new Promise(async res => {
-    console.log(`[Server | Startup] Starting thread on port: ${port}`);
-
-    const data = await thread(`./thread.js`, { PORT: port });
-
-    threads[port] = data;
-
-    console.log(`[Server | Startup] Started thread on port: ${port}`);
-
-    if(index == 0) {
-        console.log(`[Server | Startup] Generating swagger.`);
-
-        swagger({ port }).then(json => {
-            console.log(`[Server | Startup] Generated swagger.`);
-            swaggerResolve(json);
-        })
-    };
-    
-    res();
-});
-
-const threadKeys = Object.keys(threads);
-
-console.log(`[Server | Startup] Started ${threadKeys.length} threads.`);
-
-const methodsWithBody = [`POST`, `PUT`, `PATCH`];
+if (!fs.existsSync("./BloqParty-Backend.Private"))
+{
+    console.log("Execute `bun run module-init` or `bun run module-update`");
+    return;
+}
 
 const app = express();
-const server = http.createServer(async (req, res) => {
-    const timeStr = `[PROXY] ${req.url}`;
+app.use(express.json());
 
-    if(req.url.startsWith(`/docs`)) {
-        app(req, res);
-    } else if(req.url == `/pullAndRestart` && req.headers.authorization == process.env.PRIVATE_AUTH) {
-        require(`./utils/update`)().then(result => {
-            if(result == true) {
-                res.statusCode = 200;
-                res.end(`ok`);
-                process.exit(0);
-            } else {
-                res.statusCode = 500;
-                res.end(`no commits found`);
-            }
-        })
-    } else {
-        let thisThread = threadUsed++;
-        if(!threadKeys[threadUsed]) threadUsed = 0;
-    
-        const port = threadKeys[thisThread];
+registerFiles("user", false);
+registerFiles("user", true);
 
-        console.log(`[Server | API] Proxying ${req.url.split(`?`)[0]} to port: ${port}`);
+function registerFiles(category, isPrivate)
+{
+    const files = fs.readdirSync(`${isPrivate ? "./BloqParty-Backend.Private" : "./src/api"}/${category}`).filter(x => x.endsWith(".js"));
+    files.forEach((value) => {
+        const { endpoint, get, post } = require(`${isPrivate ? "../BloqParty-Backend.Private" : "./api"}/${category}/${value}`);
+        registerEndpoint(endpoint, get ?? undefined, post ?? undefined);
+    })
+}
 
-        if(methodsWithBody.includes(req.method.toUpperCase())) await new Promise(async res => {
-            let data = ``;
-
-            req.on(`end`, () => {
-                if(typeof data == `string`) {
-                    console.log(`[Server | API] request body ended before data was parsed!`);
-                    data = null;
-                    res();
-                }
-            });
-
-            req.on(`data`, chunk => {
-                if(typeof data == `string`) {
-                    data += chunk.toString();
-
-                    try {
-                        data = JSON.parse(data);
-                        req.body = data;
-                        console.log(`[Server | API] parsed body`);
-                        res();
-                    } catch(e) {
-                        console.log(`[Server | API] Error occured: ${e}`);
-                    }
-                }
-            });
-        });
-
-        const newReq = http.request(`http://localhost:${port}` + req.url, {
-            method: req.method,
-            headers: req.headers
-        }, response => {
-            res.writeHead(response.statusCode, response.headers);
-
-            let data = Buffer.alloc(0);
-
-            response.on(`data`, chunk => {
-                data = Buffer.concat([data, chunk]);
-            });
-
-            response.on(`end`, () => res.end(data));
-        });
-
-        newReq.end(JSON.stringify(req.body));
+function registerEndpoint(endpoint, get, post)
+{
+    if (get !== undefined)
+    {
+        app.get(endpoint, (req, res) => get(req, res));
+        console.log(`[Server | Endpoints] Attached GET method to "${endpoint}"`);
     }
-});
+    if (post !== undefined)
+    {
+        app.post(endpoint, (req, res) => post(req, res));
+        console.log(`[Server | Endpoints] Attached POST method to "${endpoint}"`);
+    }
+}
 
-swaggerObj.then(json => {
-    app.use(`/docs`, (req, res, next) => {
-        console.log(`[Server | Docs] ${req.url}`);
-        next();
-    }, swaggerUi.serve, swaggerUi.setup(json));
-});
-
-server.listen(parseInt(process.env.PORT), () => {
-    console.log(`[Server | Startup] Started proxy on port ${process.env.PORT}`);
-
-    fetch(process.env.NOTIF_WEBHOOK, {
-        method: "POST",
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify({
-            username: "Server Monitor Bot",
-            embeds: [ {
-                title: `Server Started`,
-                color: 0x00ff00,
-                description: `**Server:** ${process.env.DATABASE === "production" ? "Production" : "Development"}`
-            } ]
-        })
-    });
-});
+app.listen(3333, () => console.log("[Server | Setup] Started successfully on port 3333"));
